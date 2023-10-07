b8e6e85d8a4d28c8a48859dd4a11167838cb270f
devneto <miguel.neto320@gmail.com>
2023-10-07 20:16:20 -0300

    feat: create health points object

---
+++ src/domain/value-objects/HealthPoints.ts
@@ -0,0 +1,25 @@
+export class HealthPoints {
+    constructor(readonly value: number) {
+        this.value = value;
+    }
+
+    get healthPoints(): number {
+        return this.value;
+    }
+
+    reduceHealthPoints(damage: number): HealthPoints {
+        if (damage < 0) {
+            throw new Error('Damage must be greater than 0');
+        }
+
+        return HealthPoints.create(this.value - damage);
+    }
+
+    static create(hp: number): HealthPoints {
+        if (hp < 0) {
+            throw new Error('HealthPoints must be greater than 0');
+        }
+
+        return new HealthPoints(hp);
+    }
+}
