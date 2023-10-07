5e36bed66e02e06eea6ab702ab594a801d864e1b
devneto <miguel.neto320@gmail.com>
2023-10-07 20:14:43 -0300

    refactor: add type strategy in pokemon entity and ability handling

--- src/domain/entities/Pokemon.ts
+++ src/domain/entities/Pokemon.ts
@@ -1,12 +1,25 @@
-import { Id, Name, Type, Level } from "../value-objects";
+import { Id, Name, TypeStrategy, Level } from "../value-objects";
+import { Ability } from "../value-objects/Ability";
+import { HealthPoints } from "../value-objects/HealthPoints";
 
 export class Pokemon {
     constructor(
         public id: Id,
         public name: Name,
-        public type: Type,
+        public type: TypeStrategy,
         public level: Level,
-    ) { }
+        public hp: HealthPoints,
+        public maxHp: HealthPoints,
+        public Abilities: Ability[],
+    ) {
+        if (Abilities.length === 0) {
+            throw new Error("Pokemon must have at least one ability");
+        }
+
+        if (Abilities.length > 3) {
+            throw new Error("Pokemon can't have more than four abilities");
+        }
+    }
 
     get pokemonId(): number {
         return this.id.value;
@@ -17,12 +30,41 @@ export class Pokemon {
     }
 
     get pokemonType(): string {
-        return this.type.type;
+        return this.type.typeName;
     }
 
     get pokemonLevel(): number {
         return this.level.level;
     }
 
+    get pokemonHp(): number {
+        return this.hp.healthPoints;
+    }
+
+    get pokemonMaxHp(): number {
+        return this.maxHp.healthPoints;
+    }
+
+    get pokemonAbilities(): Ability[] {
+        return this.Abilities;
+    }
+
+    get firstAbility(): Ability {
+        return this.Abilities[0];
+    }
+
+    get secondAbility(): Ability {
+        return this.Abilities[1];
+    }
+
+    get thirdAbility(): Ability {
+        return this.Abilities[2];
+    }
+
+    reduceHealthPoints(damage: number): Pokemon {
+        const newHp = this.hp.reduceHealthPoints(damage);
+
+        return new Pokemon(this.id, this.name, this.type, this.level, newHp, this.maxHp, this.Abilities);
+    }
 
 }
