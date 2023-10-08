0eb14fb43b5b387f91b1fef6cf0134f21c8ec323
devneto <miguel.neto320@gmail.com>
2023-10-08 01:28:58 -0300

    refactor: add encapsulation

--- src/domain/entities/Pokemon.ts
+++ src/domain/entities/Pokemon.ts
@@ -4,45 +4,37 @@ import { HealthPoints } from "../value-objects/HealthPoints";
 
 export class Pokemon {
     constructor(
-        public id: Id,
-        public name: Name,
-        public type: TypeStrategy,
-        public level: Level,
-        public hp: HealthPoints,
-        public maxHp: HealthPoints,
-        public Abilities: Ability[],
-    ) {
-        if (Abilities.length === 0) {
-            throw new Error("Pokemon must have at least one ability");
-        }
+        private id: Id,
+        private name: Name,
+        private type: TypeStrategy,
+        private level: Level,
+        private hp: HealthPoints,
+        private maxHp: HealthPoints,
+        private Abilities: Ability[],
+    ) { }
 
-        if (Abilities.length > 3) {
-            throw new Error("Pokemon can't have more than four abilities");
-        }
+    get pokemonId() {
+        return this.id;
     }
 
-    get pokemonId(): number {
-        return this.id.value;
+    get pokemonName() {
+        return this.name;
     }
 
-    get pokemonName(): string {
-        return this.name.name;
+    get pokemonType() {
+        return this.type;
     }
 
-    get pokemonType(): string {
-        return this.type.typeName;
+    get pokemonLevel() {
+        return this.level;
     }
 
-    get pokemonLevel(): number {
-        return this.level.level;
+    get pokemonHp() {
+        return this.hp;
     }
 
-    get pokemonHp(): number {
-        return this.hp.healthPoints;
-    }
-
-    get pokemonMaxHp(): number {
-        return this.maxHp.healthPoints;
+    get pokemonMaxHp() {
+        return this.maxHp;
     }
 
     get pokemonAbilities(): Ability[] {
@@ -61,10 +53,8 @@ export class Pokemon {
         return this.Abilities[2];
     }
 
-    reduceHealthPoints(damage: number): Pokemon {
-        const newHp = this.hp.reduceHealthPoints(damage);
-
-        return new Pokemon(this.id, this.name, this.type, this.level, newHp, this.maxHp, this.Abilities);
+    get fourthAbility(): Ability {
+        return this.Abilities[3];
     }
 
 }
