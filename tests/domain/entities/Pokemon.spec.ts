99763d675490db59e6bd8d918faa0ec9bfa418d1
devneto <miguel.neto320@gmail.com>
2023-10-07 22:58:42 -0300

    test: sample pokemon test

--- tests/domain/entities/Pokemon.spec.ts
+++ tests/domain/entities/Pokemon.spec.ts
@@ -2,6 +2,8 @@ import { it, describe, expect } from "vitest";
 
 import { Pokemon } from "../../../src/domain/entities/Pokemon";
 import { Grass, Id, Level, Name } from "../../../src/domain/value-objects";
+import { HealthPoints } from "../../../src/domain/value-objects/HealthPoints";
+import { Ability } from "../../../src/domain/value-objects/Ability";
 
 
 describe("Pokemon", () => {
@@ -11,15 +13,24 @@ describe("Pokemon", () => {
         const name = new Name("Bulbasaur");
         const type = new Grass();
         const level = new Level(1);
+        const hp = new HealthPoints(300)
+        const maxHp = new HealthPoints(300)
+        const abilities = [
+            new Ability("Overgrow", "When HP is below 1/3rd, Grass's power increases to 1.5 times.", 20),
+            new Ability("Chlorophyll", "When sunny, the Pokémon's Speed doubles.", 20),
+            new Ability("Leaf Guard", "Prevents status conditions in sunny weather.", 20),
+        ]
 
         // Act
-        const pokemon = new Pokemon(id, name, type, level);
+        const pokemon = new Pokemon(id, name, type, level, hp, maxHp, abilities);
 
         // Assert
         expect(pokemon.pokemonId).toBe(id.value);
         expect(pokemon.pokemonName).toBe(name.value);
-        expect(pokemon.pokemonType).toBe(type.value);
+        expect(pokemon.pokemonType).toBe(type.typeName);
         expect(pokemon.pokemonLevel).toBe(level.value);
+        expect(pokemon.hp.healthPoints).toBe(hp.healthPoints);
+        expect(pokemon.Abilities).toBe(abilities);
     });
 
 });
