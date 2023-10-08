f52eb00c92382cee3f15328d192188a4a34687d9
devneto <miguel.neto320@gmail.com>
2023-10-08 01:28:45 -0300

    refactor: add encapsulation

--- src/domain/entities/Battle.ts
+++ src/domain/entities/Battle.ts
@@ -4,20 +4,68 @@ import { Pokemon } from "./Pokemon";
 export class Battle {
 
     constructor(
-        readonly pokemon1: Pokemon,
-        readonly pokemon2: Pokemon,
-        readonly turns: Turn[],
+        private readonly pokemon1: Pokemon,
+        private readonly pokemon2: Pokemon,
+        private readonly currentPokemon: Pokemon,
+        private readonly turns: Turn[],
     ) { }
+
+    get battlePokemon1(): Pokemon {
+        return this.pokemon1;
+    }
+
+    get battlePokemon2(): Pokemon {
+        return this.pokemon2;
+    }
+
+    get battleCurrentPokemon(): Pokemon {
+        return this.currentPokemon;
+    }
+
+    get battleTurns(): Turn[] {
+        return this.turns;
+    }
 
 
     static create(pokemon1: Pokemon, pokemon2: Pokemon): Battle {
-        return new Battle(pokemon1, pokemon2, []);
+        return new Battle(pokemon1, pokemon2, pokemon1, []);
     }
 
     addTurn(turn: Turn): Battle {
+        if (this.currentPokemon.pokemonId !== turn.attacker.pokemonId) {
+            throw new Error('The attacker is not the current pokemon');
+        }
+
+        const newCurrentPokemon = this.getNextPokemon();
 
         const { pokemon1, pokemon2 } = turn.execute();
 
-        return new Battle(pokemon1, pokemon2, [...this.turns, turn]);
+        return new Battle(pokemon1, pokemon2, newCurrentPokemon, [...this.turns, turn]);
     }
+
+    isBattleFinished(): boolean {
+        return this.pokemon1.pokemonHp.healthPoints === 0 || this.pokemon2.pokemonHp.healthPoints === 0;
+    }
+
+    getWinner(): Pokemon | null {
+        if (!this.isBattleFinished()) {
+            return null;
+        }
+
+        return this.pokemon1.pokemonHp.healthPoints === 0 ? this.pokemon2 : this.pokemon1;
+    }
+
+    getLoser(): Pokemon | null {
+        if (!this.isBattleFinished()) {
+            return null;
+        }
+
+        return this.pokemon1.pokemonHp.healthPoints === 0 ? this.pokemon1 : this.pokemon2;
+    }
+
+    private getNextPokemon(): Pokemon {
+        return this.currentPokemon.pokemonId === this.pokemon1.pokemonId ? this.pokemon2 : this.pokemon1;
+    }
+
+
 }
