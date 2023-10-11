b628b67bd51b637bd50c738e6775e8ccbc1c631c
devneto <miguel.neto320@gmail.com>
2023-10-10 23:31:22 -0300

    feat: create id value object

--- src/domain/value-objects/Id.ts
+++ src/domain/value-objects/Id.ts
@@ -3,6 +3,14 @@ export class Id {
         private readonly value: number,
     ) { }
 
+    static create(id: number): Id {
+        if (id < 0) {
+            throw new Error('Id must be greater than 0');
+        }
+
+        return new Id(id);
+    }
+
     get id(): number {
         return this.value;
     }
