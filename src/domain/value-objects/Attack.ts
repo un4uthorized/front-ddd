1db33fe76bcf8bf0ff7a65c4f9db1947ac7d9931
devneto <miguel.neto320@gmail.com>
2023-10-10 23:24:03 -0300

    feat: create attack stats value object

--- src/domain/value-objects/Attack.ts
+++ src/domain/value-objects/Attack.ts
@@ -1,41 +1,13 @@
-import { Ability, HealthPoints } from ".";
-import { Pokemon } from "../entities";
-import { Turn } from "./Turn";
+export class Attack {
+    constructor(private readonly value: number) { }
 
-export class Attack implements Turn {
-    constructor(
-        readonly attacker: Pokemon,
-        readonly defender: Pokemon,
-        readonly ability: Ability
-    ) {
-        if (!attacker.pokemonAbilities.includes(ability)) {
-            throw new Error('This pokemon does not have this ability');
-        }
+    static create(value: number): Attack {
+        if (value < 0) throw new Error('Attack must be a positive number');
+
+        return new Attack(value);
     }
 
-
-    static create(pokemon1: Pokemon, pokemon2: Pokemon, ability: Ability): Attack {
-        return new Attack(pokemon1, pokemon2, ability);
-    }
-
-    execute(): { pokemon1: Pokemon, pokemon2: Pokemon } {
-        const damage = this.ability.abilityDamage;
-
-        const newHealthPointsAfterAttack = HealthPoints.create(this.defender.pokemonHp.healthPoints - damage)
-
-        const pokemonAfterAttack = new Pokemon(
-            this.defender.pokemonId,
-            this.defender.pokemonName,
-            this.defender.pokemonType,
-            this.defender.pokemonLevel,
-            newHealthPointsAfterAttack,
-            this.defender.pokemonMaxHp,
-            this.defender.pokemonAbilities
-        );
-
-        return {
-            pokemon1: this.attacker,
-            pokemon2: pokemonAfterAttack
-        }
+    get attack(): number {
+        return this.value;
     }
 }
