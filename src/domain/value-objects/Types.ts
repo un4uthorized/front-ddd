84ea38c23f1871049fb43c16e769e7dbaa7a63e0
devneto <miguel.neto320@gmail.com>
2023-10-10 23:33:35 -0300

    feat: add types namespace

---
+++ src/domain/value-objects/Types.ts
@@ -0,0 +1,9 @@
+import { Grass } from "./Grass";
+import { Fire } from "./Fire"
+import { Water } from "./Water";
+
+export class Types {
+    static GRASS = new Grass();
+    static FIRE = new Fire();
+    static WATER = new Water();
+}
