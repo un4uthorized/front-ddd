8ca759a696fac6346046fe4538777aba5a0f8d4f
devneto <miguel.neto320@gmail.com>
2023-10-10 23:32:52 -0300

    feat: add static create name

--- src/domain/value-objects/Name.ts
+++ src/domain/value-objects/Name.ts
@@ -3,6 +3,10 @@ export class Name {
         private readonly value: string,
     ) { }
 
+    static create(name: string): Name {
+        return new Name(name);
+    }
+
     get name(): string {
         return this.value;
     }
